211e8e81f6079ff3963b02f0a4339667541df66a
Andre Luiz de Souza <andresou25@gmail.com>
2020-08-06 18:42:32 -0300

    nossa primeira api

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send( {message: 'Olá mundo esse é novo respositório clonado'});
+});
+app.listen( 4001, () => {
+    console.log('Api inicializada na porta 4001');
+});
